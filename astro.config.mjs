bfdda5ee567a5021509b4cd9de24561930c160ff
ChaponanWill <chapowilliam09@gmail.com>
2025-04-08 19:33:33 -0500

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site:'https://chaponanwill.github.io',
+    base:'target-challenge',
     vite: {
         plugins: [tailwindcss()],
     },
-    site:*'https://chaponanwill.github.io'*,
-    base:*'target-challenge'*
 });
